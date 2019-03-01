c4909e6b9fcc7d6d9a0ae811f27712fc27dbd60f
José Israel Minguela <48068420+CryptoExtrem@users.noreply.github.com>
2019-03-02 00:10:40 +0100

    Fork TipBot
    TipBot for Telegram forked

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# PlanetPay_TipBot
+# PlanetPay Tipbot for Telegram -
+
+#### Forked and mixed from:
+####  https://github.com/DigitalCoin1/TipBot_SperoCoin
+####  https://github.com/samgos/reddbot-telegram
+####  https://github.com/flokisatoshi/IdealCash-Telegram-Tipbot
+
+## Dependencies
+
+*  `apt-get install python3`
+*  `apt-get install python3-pip`
+*  `pip3 install beautifulsoup4`
+*  `pip3 install python-telegram-bot --upgrade`
+
+* In order to run the tip-bot effectively, a Bitcoin-core based client is needed. For this Planetpayd is used
+
+## Setup
+
+* Install the deps
+* Install planetpayd to /home/wallet/planetpay/src/Planetpayd
+* Run and exit program
+### NOTE if not using Planetpayd compiled from the experimental branch of the github, you will have to provide the planetpay.conf file and place it manually in the ~/.Planetpay/ directory.
+* edit the planetpay.conf file located at ~/.Planetpay/planetpay.conf to contain the following:
+
+* `staking=0`
+* `enableaccounts=1`
+### Staking cannot be enabled while running for the tipbot.
+
+* Run planetpayd and let it start syncing
+
+
+* Setup a bot with the user @BotFather through PM on Telegram, after going through a setup you will be given a bot token. Edit the command.py file and replace the parameter 'BOT_TOKEN_HERE' with the one you just recieved.
+
+*  Run the script
+`python3 command.py`
+
+*  Initiate the bot by inviting it to a chat or via PM, some commands are `/hi` , `/commands` , `/deposit` , `/withdraw` , `/balance` and to find out the format related to tip others and withdrawal of funds use `/help`.
+
+### Setting up the bot as so still leaves the wallet unencrypted, so please go to extra measures to provide extra security. Make sure to have SSH encryption on whatever device/droplet you run it on.
+
+*  Please fork the code, happy tipping!
---
+++ command.py
@@ -0,0 +1,129 @@
+#-*- coding: utf-8 -*-
+
+import json
+import codecs
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+import re
+import subprocess
+from telegram.ext.dispatcher import run_async
+from telegram.ext import Updater
+from html import escape
+
+updater = Updater(token='BOT_TOKEN_HERE')
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+					level=logging.INFO)
+
+def commands(bot, update):
+	user = update.message.from_user.username
+	bot.send_message(chat_id=update.message.chat_id, text="Initiating commands /tip & /withdraw have a specfic format,\n use them like so:" + "\n \n Parameters: \n <user> = target user to tip \n <amount> = amount of PlanetPay to utilise \n <address> = PlanetPay address to withdraw to \n \n Tipping format: \n /tip <user> <amount> \n \n Withdrawing format: \n /withdraw <address> <amount>")
+
+def help(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text="The following commands are at your disposal: /hi , /commands , /deposit , /tip , /withdraw , or /balance")
+
+def deposit(bot, update):
+	user = update.message.from_user.username
+	if user is None:
+		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+	else:
+		address = "/home/wallet/planetpay/src/Planetpayd"
+		result = subprocess.run([address,"getaccountaddress",user],stdout=subprocess.PIPE)
+		clean = (result.stdout.strip()).decode("utf-8")
+		bot.send_message(chat_id=update.message.chat_id, text="@{0} your depositing address is: {1}".format(user,clean))
+
+def tip(bot,update):
+	user = update.message.from_user.username
+	target = update.message.text[5:]
+	amount =  target.split(" ")[1]
+	target =  target.split(" ")[0]
+	if user is None:
+		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+	else:
+		machine = "@PlanetPay_TipBot"
+		if target == machine:
+			bot.send_message(chat_id=update.message.chat_id, text="HODL.")
+		elif "@" in target:
+			target = target[1:]
+			user = update.message.from_user.username
+			core = "/home/wallet/planetpay/src/Planetpayd"
+			result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
+			balance = float((result.stdout.strip()).decode("utf-8"))
+			amount = float(amount)
+			if balance < amount:
+				bot.send_message(chat_id=update.message.chat_id, text="@{0} you have insufficent funds.".format(user))
+			elif target == user:
+				bot.send_message(chat_id=update.message.chat_id, text="You can't tip yourself!")
+			else:
+				balance = str(balance)
+				amount = str(amount)
+				tx = subprocess.run([core,"move",user,target,amount],stdout=subprocess.PIPE)
+				bot.send_message(chat_id=update.message.chat_id, text="@{0} tipped @{1} of {2} PLT".format(user, target, amount))
+		else:
+			bot.send_message(chat_id=update.message.chat_id, text="Error that user is not applicable.")
+
+def balance(bot,update):
+	user = update.message.from_user.username
+	if user is None:
+		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+	else:
+		core = "/home/wallet/planetpay/src/Planetpayd"
+		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
+		clean = (result.stdout.strip()).decode("utf-8")
+		balance  = float(clean)
+		balance =  str(round(balance,3))
+		bot.send_message(chat_id=update.message.chat_id, text="@{0} your current balance is: {1} PLT ".format(user,balance))
+
+
+
+def withdraw(bot,update):
+	user = update.message.from_user.username
+	if user is None:
+		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+	else:
+		target = update.message.text[9:]
+		address = target[:35]
+		address = ''.join(str(e) for e in address)
+		target = target.replace(target[:35], '')
+		amount = float(target)
+		core = "/home/wallet/planetpay/src/Planetpayd"
+		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
+		clean = (result.stdout.strip()).decode("utf-8")
+		balance = float(clean)
+		if balance < amount:
+			bot.send_message(chat_id=update.message.chat_id, text="@{0} you have insufficent funds.".format(user))
+		else:
+			amount = str(amount)
+			tx = subprocess.run([core,"sendfrom",user,address,amount],stdout=subprocess.PIPE)
+			bot.send_message(chat_id=update.message.chat_id, text="@{0} has successfully withdrew to address: {1} of {2} PLT" .format(user,address,amount))
+
+def hi(bot,update):
+	user = update.message.from_user.username
+	bot.send_message(chat_id=update.message.chat_id, text="Hello @{0}, I like PlanetPay, and you?".format(user))
+
+from telegram.ext import CommandHandler
+
+commands_handler = CommandHandler('commands', commands)
+dispatcher.add_handler(commands_handler)
+
+hi_handler = CommandHandler('hi', hi)
+dispatcher.add_handler(hi_handler)
+
+withdraw_handler = CommandHandler('withdraw', withdraw)
+dispatcher.add_handler(withdraw_handler)
+
+deposit_handler = CommandHandler('deposit', deposit)
+dispatcher.add_handler(deposit_handler)
+
+tip_handler = CommandHandler('tip', tip)
+dispatcher.add_handler(tip_handler)
+
+balance_handler = CommandHandler('balance', balance)
+dispatcher.add_handler(balance_handler)
+
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
+
+updater.start_polling()
